ad6314647a771bc5e1800d7cb32a456b59cefba6
Yentl Valencia <yvalencia@exist.com>
2024-05-15 12:54:38 +0800

    removed unused imports

--- src/main/java/com/example/helpdesk/repository/TicketRepository.java
+++ src/main/java/com/example/helpdesk/repository/TicketRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.helpdesk.model.Ticket;
-import com.example.helpdesk.model.enums.Status;
 
 import jakarta.transaction.Transactional;
 
