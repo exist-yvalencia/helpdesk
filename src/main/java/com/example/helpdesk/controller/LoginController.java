76e739f7ea0461b28b16c468ae1c0fcd37fd7b63
Yentl Valencia <yvalencia@exist.com>
2024-05-15 14:00:48 +0800

    make role as return value for login

--- src/main/java/com/example/helpdesk/controller/LoginController.java
+++ src/main/java/com/example/helpdesk/controller/LoginController.java
@@ -1,8 +1,12 @@
 package com.example.helpdesk.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +14,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/login")
 public class LoginController {
+    @Autowired
+    private Environment env;
+
     @GetMapping
     @PreAuthorize("permitAll()")
     public ResponseEntity<String> login() {
-        return new ResponseEntity<>("Success", HttpStatus.OK);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if(authentication.getName() == env.getProperty("spring.security.user.name")){
+            return new ResponseEntity<>("USER", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("ADMIN", HttpStatus.OK);
     }
 }
