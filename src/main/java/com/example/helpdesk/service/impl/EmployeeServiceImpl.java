672bfc11841b722305099d0c66c6d198b3523e1c
Yentl Valencia <yvalencia@exist.com>
2024-05-15 12:52:17 +0800

    renamed typo in class name

--- src/main/java/com/example/helpdesk/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/example/helpdesk/service/impl/EmployeeServiceImpl.java
@@ -3,31 +3,37 @@ package com.example.helpdesk.service.impl;
 import java.util.List;
 import java.util.NoSuchElementException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.example.helpdesk.model.Employee;
+import com.example.helpdesk.model.enums.Status;
 import com.example.helpdesk.repository.EmployeeRepository;
 import com.example.helpdesk.repository.TicketRepository;
 import com.example.helpdesk.service.EmployeeService;
 
 @Service
-public class EmployeeSeviceImpl implements EmployeeService {
+public class EmployeeServiceImpl implements EmployeeService {
     @Autowired
     EmployeeRepository employeeRepository;
     @Autowired
     TicketRepository ticketRepository;
+    Logger logger;
 
-    public EmployeeSeviceImpl(EmployeeRepository employeeRepository, TicketRepository ticketRepository) {
+    public EmployeeServiceImpl(EmployeeRepository employeeRepository, TicketRepository ticketRepository) {
         this.employeeRepository = employeeRepository;
         this.ticketRepository = ticketRepository;
+        this.logger = LoggerFactory.getLogger(EmployeeServiceImpl.class);
     }
 
     @Override
     public ResponseEntity<Employee> getEmployee(String id) {
         try {
+            logger.debug("getEmployee("+id+")");
             return new ResponseEntity<>(employeeRepository.findById(Integer.valueOf(id)).get(), HttpStatus.OK);
         } catch(NoSuchElementException e) {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
@@ -37,6 +43,7 @@ public class EmployeeSeviceImpl implements EmployeeService {
     @Override
     public ResponseEntity<List<Employee>> getEmployeeList() {
         try {
+            logger.debug("getEmployeeList()");
             return new ResponseEntity<>(employeeRepository.findAll(), HttpStatus.OK);
         } catch(Exception e) {
             return new ResponseEntity<>(null, HttpStatus.NOT_FOUND);
@@ -49,7 +56,7 @@ public class EmployeeSeviceImpl implements EmployeeService {
             employeeRepository.save(employee);
             return new ResponseEntity<>("Employee saved", HttpStatus.OK);
         }else {
-            return new ResponseEntity<>("Employee already exists", HttpStatus.CONFLICT);
+            return new ResponseEntity<>("Employee already exists", HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -59,12 +66,12 @@ public class EmployeeSeviceImpl implements EmployeeService {
             employeeRepository.findById(Integer.valueOf(employeeId)).get();
             ticketRepository.findById(Integer.valueOf(ticketNumber)).get();
 
-            ticketRepository.updateAssignee(Integer.parseInt(employeeId), Integer.parseInt(ticketNumber));
+            ticketRepository.updateAssignee(Integer.parseInt(employeeId), Status.ASSIGNED.getValue(), Integer.parseInt(ticketNumber));
             return new ResponseEntity<>("Ticket assigned to employee successfully", HttpStatus.OK);
         } catch(NoSuchElementException e) {
             return new ResponseEntity<>("Employee/Ticket does not exist", HttpStatus.NOT_FOUND);
         } catch (Exception e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_MODIFIED);
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
     }
 
@@ -73,19 +80,19 @@ public class EmployeeSeviceImpl implements EmployeeService {
         try {
             Employee foundEmployee = employeeRepository.findById(employee.getId()).get();
 
-            if(employee.getEmployeeNumber() != null) {
+            if(employee.getEmployeeNumber() != null && !employee.getEmployeeNumber().isEmpty()) {
                 foundEmployee.setEmployeeNumber(employee.getEmployeeNumber());
             }
 
-            if(employee.getFirstName() != null) {
+            if(employee.getFirstName() != null && !employee.getFirstName().isEmpty()) {
                 foundEmployee.setFirstName(employee.getFirstName());
             }
 
-            if(employee.getMiddleName() != null) {
+            if(employee.getMiddleName() != null && !employee.getMiddleName().isEmpty()) {
                 foundEmployee.setMiddleName(employee.getMiddleName());
             }
 
-            if(employee.getLastName() != null) {
+            if(employee.getLastName() != null && !employee.getLastName().isEmpty()) {
                 foundEmployee.setLastName(employee.getLastName());
             }
 
@@ -115,7 +122,7 @@ public class EmployeeSeviceImpl implements EmployeeService {
         } catch(NoSuchElementException e) {
             return new ResponseEntity<>("Employee does not exist", HttpStatus.NOT_FOUND);
         } catch (Exception e) {
-            return new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.NOT_IMPLEMENTED);
+            return new ResponseEntity<>(e.getLocalizedMessage(), HttpStatus.BAD_REQUEST);
         }
         
     }
